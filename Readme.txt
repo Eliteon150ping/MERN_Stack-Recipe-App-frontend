1f516f4dbae43afd934a82174ffe218d6e6ad07f
Rishen <rishensurjoobhalee@example.com>
2024-06-28 16:30:07 +0200

    frontend

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+echo "# MERN_Stack-Recipe-App-frontend" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Eliteon150ping/MERN_Stack-Recipe-App-frontend.git
+git push -u origin main
---
+++ src/pages/saved-recipes.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+
+export const SavedRecipes = () => {
+  const [savedRecipes, setSavedRecipes] = useState([]);
+  const userID = useGetUserID();
+  const [cookies, _] = useCookies(["access_token"]);
+
+  useEffect(() => {
+    const fetchSavedRecipes = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/recipes/user/${userID}`,
+          {
+            headers: { authorization: `Bearer ${cookies.access_token}` },
+          }
+        );
+        setSavedRecipes(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchSavedRecipes();
+  }, [userID, cookies.access_token]);
+
+  return (
+    <div>
+      <h1>My Recipes</h1>
+      <ul>
+        {savedRecipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+            </div>
+            <p>{recipe.instructions}</p>
+            <p>{recipe.description}</p>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Ingredients: {recipe.ingredients.join(", ")}</p>
+            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
